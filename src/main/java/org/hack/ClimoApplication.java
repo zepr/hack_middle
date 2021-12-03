ce2b92903fcb5daf7213201c43bff1f51ea3545c
Glenn Sanson <glenn.sanson@free.fr>
2021-12-03 18:28:27 +0100

    Initialisation du repo

---
+++ src/main/java/org/hack/ClimoApplication.java
@@ -0,0 +1,13 @@
+package org.hack;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClimoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ClimoApplication.class, args);
+    }
+
+}
