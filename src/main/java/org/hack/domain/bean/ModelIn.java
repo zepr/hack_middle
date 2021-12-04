0719023e9401a556fba3516a364681bb98a889f0
Glenn Sanson <glenn.sanson@free.fr>
2021-12-04 11:24:46 +0100

    Ajout système de recherche de ville + input depuis front

--- src/main/java/org/hack/domain/bean/ModelIn.java
+++ src/main/java/org/hack/domain/bean/ModelIn.java
@@ -1,4 +1,16 @@
 package org.hack.domain.bean;
 
+import lombok.Data;
+
+@Data
 public class ModelIn {
+    private String lieu;
+    private String reserveUtileEau;
+    private String pmgMax;
+    private String tempOptimale;
+    private String coutAssurance;
+    private String prixDeVente;
+    private String tempMax;
+    private String rendementMax;
+    private String epSol;
 }
--- src/main/java/org/hack/service/ModelService.java
+++ src/main/java/org/hack/service/ModelService.java
@@ -2,10 +2,12 @@ package org.hack.service;
 
 import lombok.RequiredArgsConstructor;
 import org.hack.domain.ModelLoader;
+import org.hack.domain.TownSearch;
 import org.hack.domain.bean.ModelIn;
 import org.hack.domain.bean.ModelOut;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
@@ -15,8 +17,13 @@ import java.util.List;
 public class ModelService {
     private final ModelLoader loader;
 
-    @GetMapping(value = "/model", produces = MediaType.APPLICATION_JSON_VALUE)
+    @GetMapping(value = "/api/model", produces = MediaType.APPLICATION_JSON_VALUE)
     public List<ModelOut> call() {
         return loader.getData(new ModelIn());
+    }
+
+    @GetMapping(value = "/api/find/{code}", produces = MediaType.APPLICATION_JSON_VALUE)
+    public List<String> getTown(@PathVariable String code) {
+        return TownSearch.getTown(code);
     }
 }
